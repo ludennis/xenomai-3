e35ec469a9d7e2d3ab7751fe39538d009eded19a
Dennis Lu <Dennis.Lu@itri.org.tw>
2020-02-04 15:53:53 +0800

    controller_main: added skeleton code

--- src/motor_model_v2_node/src/controller_code/controller_main.cpp
+++ src/motor_model_v2_node/src/controller_code/controller_main.cpp
@@ -1,6 +1,21 @@
 #include <dds/dds.hpp>
 
+class SubEntities
+{
+public:
+private:
+};
 int main()
 {
+  // domain participant
+
+  // topic
+
+  // publisher
+
+  // data writer
+
+  // data reader
+
   return 0;
 }
