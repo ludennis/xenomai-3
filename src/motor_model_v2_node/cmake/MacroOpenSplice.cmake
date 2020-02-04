56b13646eece4e2a5b5f3b378894e2d4094ddd27
Dennis Lu <Dennis.Lu@itri.org.tw>
2020-02-04 15:53:22 +0800

    CMakeLists.txt: added MacroOpenSplice.cmake and osmacros.cmake

--- src/motor_model_v2_node/cmake/MacroOpenSplice.cmake
+++ src/motor_model_v2_node/cmake/MacroOpenSplice.cmake
@@ -1,22 +1,26 @@
 MACRO(DEFINE_OpenSplice_SOURCES idlfilename)
   SET(outsources)
   GET_FILENAME_COMPONENT(it ${idlfilename} ABSOLUTE)
+  GET_FILENAME_COMPONENT(idl_dir ${idlfilename} DIRECTORY)
   GET_FILENAME_COMPONENT(nfile ${idlfilename} NAME_WE)
-  SET(outsources ${outsources} gen/${nfile}.cpp gen/${nfile}.h)
-  SET(outsources ${outsources} gen/${nfile}Dcps.cpp gen/${nfile}Dcps.h)
-  SET(outsources ${outsources} gen/${nfile}Dcps_impl.cpp gen/${nfile}Dcps_impl.h)
-  SET(outsources ${outsources} gen/${nfile}SplDcps.cpp gen/${nfile}SplDcps.h)
-  SET(outsources ${outsources} gen/ccpp_${nfile}.h)
+  SET(outsources ${outsources} ${idl_dir}/gen/${nfile}.cpp ${idl_dir}/gen/${nfile}.h)
+  SET(outsources ${outsources} ${idl_dir}/gen/${nfile}Dcps.cpp ${idl_dir}/gen/${nfile}Dcps.h)
+  SET(outsources ${outsources} ${idl_dir}/gen/${nfile}Dcps_impl.cpp ${idl_dir}/gen/${nfile}Dcps_impl.h)
+  SET(outsources ${outsources} ${idl_dir}/gen/${nfile}SplDcps.cpp ${idl_dir}/gen/${nfile}SplDcps.h)
+  SET(outsources ${outsources} ${idl_dir}/gen/ccpp_${nfile}.h)
 ENDMACRO(DEFINE_OpenSplice_SOURCES)
 
 MACRO(OpenSplice_IDLGEN idlfilename)
   GET_FILENAME_COMPONENT(it ${idlfilename} ABSOLUTE)
   GET_FILENAME_COMPONENT(idlfilename ${idlfilename} NAME)
   DEFINE_OpenSplice_SOURCES(${ARGV})
+
   ADD_CUSTOM_COMMAND (
+    PRE_BUILD
     OUTPUT ${outsources}
     COMMAND ${OpenSplice_IDLGEN_BINARY}
-    ARGS -S -l cpp -d gen ${idlfilename}
+    ARGS -S -l cpp -d ${PROJECT_SOURCE_DIR}/src/idl/gen ${idlfilename}
     DEPENDS ${it}
+    COMMENT "${outsources} have been produced in directory gen"
   )
 ENDMACRO(OpenSplice_IDLGEN)
