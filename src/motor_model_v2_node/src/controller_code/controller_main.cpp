f252ef33839bc21bdcc450652345bc90deb533f8
Dennis Lu <Dennis.Lu@itri.org.tw>
2020-02-05 13:33:12 +0800

    idl: merged ControlMessage.idl and MotorMessage.idl into MotorControllerUnitModule.idl

--- src/motor_model_v2_node/src/controller_code/controller_main.cpp
+++ src/motor_model_v2_node/src/controller_code/controller_main.cpp
@@ -3,7 +3,7 @@
 
 #include <dds/dds.hpp>
 
-#include <ControlMessage_DCPS.hpp>
+#include <MotorControllerUnitModule_DCPS.hpp>
 
 int main()
 {
@@ -11,14 +11,20 @@ int main()
   dds::domain::DomainParticipant participant(1);
 
   // topic
-  dds::topic::Topic<ControlMessage> topic(participant, "control_topic");
+  dds::topic::Topic<MotorControllerUnitModule::ControlMessage> contorlTopic(participant, "control_topic");
 
   // publisher
   dds::pub::Publisher publisher(participant);
 
   // data writer
-  dds::pub::DataWriter<ControlMessage> writer(publisher, topic);
+  dds::pub::DataWriter<MotorControllerUnitModule::ControlMessage> writer(publisher,contorlTopic);
 
+  for(auto count{1ll};;++count)
+  {
+    // send control signals to motor
+    writer << MotorControllerUnitModule::ControlMessage(1, "ping");
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+  }
 
   // data reader
 
