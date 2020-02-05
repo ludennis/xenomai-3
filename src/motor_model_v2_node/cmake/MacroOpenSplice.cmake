0faa1ae42be594690be68ab2442971935044b4b1
Dennis Lu <Dennis.Lu@itri.org.tw>
2020-02-05 10:59:47 +0800

    CMakeLists.txt: fixed LNK2019 error of having unresolved extenal symbols

--- src/motor_model_v2_node/cmake/MacroOpenSplice.cmake
+++ src/motor_model_v2_node/cmake/MacroOpenSplice.cmake
@@ -4,8 +4,7 @@ MACRO(DEFINE_OpenSplice_SOURCES idlfilename)
   GET_FILENAME_COMPONENT(idl_dir ${idlfilename} DIRECTORY)
   GET_FILENAME_COMPONENT(nfile ${idlfilename} NAME_WE)
   SET(outsources ${outsources} ${idl_dir}/gen/${nfile}.cpp ${idl_dir}/gen/${nfile}.h)
-  SET(outsources ${outsources} ${idl_dir}/gen/${nfile}Dcps.cpp ${idl_dir}/gen/${nfile}Dcps.h)
-  SET(outsources ${outsources} ${idl_dir}/gen/${nfile}Dcps_impl.cpp ${idl_dir}/gen/${nfile}Dcps_impl.h)
+  SET(outsources ${outsources} ${idl_dir}/gen/${nfile}_DCPS.cpp ${idl_dir}/gen/${nfile}_DCPS.h)
   SET(outsources ${outsources} ${idl_dir}/gen/${nfile}SplDcps.cpp ${idl_dir}/gen/${nfile}SplDcps.h)
   SET(outsources ${outsources} ${idl_dir}/gen/ccpp_${nfile}.h)
 ENDMACRO(DEFINE_OpenSplice_SOURCES)
@@ -19,7 +18,7 @@ MACRO(OpenSplice_IDLGEN idlfilename)
     PRE_BUILD
     OUTPUT ${outsources}
     COMMAND ${OpenSplice_IDLGEN_BINARY}
-    ARGS -S -l isocpp2 -d ${PROJECT_SOURCE_DIR}/src/idl/gen ${idlfilename}
+    ARGS -l isocpp2 -d ${PROJECT_SOURCE_DIR}/src/idl/gen ${idlfilename}
     DEPENDS ${it}
     COMMENT "${outsources} have been produced in directory gen"
   )
--- src/motor_model_v2_node/src/controller_code/controller_main.cpp
+++ src/motor_model_v2_node/src/controller_code/controller_main.cpp
@@ -1,19 +1,24 @@
+#include <chrono>
+#include <thread>
+
 #include <dds/dds.hpp>
 
-class SubEntities
-{
-public:
-private:
-};
+#include <ControlMessage_DCPS.hpp>
+
 int main()
 {
   // domain participant
+  dds::domain::DomainParticipant participant(1);
 
   // topic
+  dds::topic::Topic<ControlMessage> topic(participant, "control_topic");
 
   // publisher
+  dds::pub::Publisher publisher(participant);
 
   // data writer
+  dds::pub::DataWriter<ControlMessage> writer(publisher, topic);
+
 
   // data reader
 
