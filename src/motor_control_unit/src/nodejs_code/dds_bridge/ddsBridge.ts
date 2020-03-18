1e923625ad5d5cb70ed7453db679884d716e993c
Dennis Lu <Dennis.Lu@itri.org.tw>
2020-03-18 10:34:40 +0800

    ddsBridge.ts: added skeleton code with require of vortexdds and path

--- src/motor_control_unit/src/nodejs_code/dds_bridge/ddsBridge.ts
+++ src/motor_control_unit/src/nodejs_code/dds_bridge/ddsBridge.ts
@@ -1,6 +1,21 @@
+import path = require("path");
+import dds = require("vortexdds");
+
 main();
 
 function main() {
+  /* finds and loads the IDL file */
+  const idlFileDirectory = "../../idl/"
+  const idlFilename = "MotorControllerUnitModule.idl";
+  LoadIDLFile(idlFileDirectory, idlFilename);
+
   console.log('finished');
   process.exit(0);
+}
+
+function LoadIDLFile(idlFileDirectory: string, idlFilename: string) {
+  console.log("looking into directory: " + idlFileDirectory);
+  console.log("looking for file: " + idlFilename);
+
+  return dds.importIDL(idlFileDirectory + idlFilename);
 }
