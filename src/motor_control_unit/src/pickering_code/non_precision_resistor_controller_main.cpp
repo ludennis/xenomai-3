f79d8b0be9cc2caac5c4719c29f2b2ad4aedd9ac
Dennis Lu <Dennis.Lu@itri.org.tw>
2020-05-06 14:56:24 +0800

    non_precision_resistor_controller_main.cpp: added functionality to find specified card

---
+++ src/motor_control_unit/src/pickering_code/non_precision_resistor_controller_main.cpp
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+
+#include <Pilpxi.h>
+
+int main(int argc, char **argv)
+{
+  DWORD numOfFreeCards;
+  DWORD buses[100]; // assume theres a maximum of 100 cards
+  DWORD devices[100]; // assume theres a maximum of 100 cards
+  DWORD bus;
+  DWORD device;
+  DWORD cardNum;
+  DWORD err;
+  DWORD resistance;
+  CHAR id[100];
+
+  if(argc == 1)
+  {
+    // find free cards
+    PIL_CountFreeCards(&numOfFreeCards);
+    printf("Found %d free cards\n", numOfFreeCards);
+    PIL_FindFreeCards(numOfFreeCards, buses, devices);
+    printf("Card addresses:\n");
+    for(auto i{0}; i < numOfFreeCards; ++i)
+    {
+      printf("Opening card at bus %d, device %d\n", buses[i], devices[i]);
+      err = PIL_OpenSpecifiedCard(buses[i], devices[i], &cardNum);
+      if(err == 0)
+      {
+        printf("Card Number is %d\n", cardNum);
+        PIL_CardId(cardNum, id);
+        printf(" Card id is %s\n", id);
+        PIL_CloseSpecifiedCard(cardNum);
+      }
+      else
+      {
+        printf("Error return is %d\n", err);
+      }
+    }
+  }
+  else if(argc == 4)
+  {
+    bus = std::atoi(argv[1]);
+    device = std::atoi(argv[2]);
+    resistance = std::atoi(argv[3]);
+
+    printf("Accessing bus %d, device %d, target resistance %d\n", bus, device, resistance);
+
+    // check target id is a resistor
+    err = PIL_OpenSpecifiedCard(bus, device, &cardNum);
+
+    if(err == 0)
+    {
+      printf("Card Number is %d\n", cardNum);
+      PIL_CardId(cardNum, id);
+      printf("Card id is %s\n", id);
+
+      // check if the card is a resistor
+      std::string cardId(id);
+      std::string resistorId("40-295-121");
+      if(cardId.find(resistorId) != std::string::npos)
+      {
+        printf("it has resistor id(%s)!\n", resistorId.c_str());
+      }
+      else
+      {
+        printf("Card doesn't have resistor id (%s), might not be a resistor card\n",
+          resistorId.c_str());
+      }
+
+
+      PIL_CloseSpecifiedCard(cardNum);
+    }
+    else
+    {
+      printf("error return is %d\n", err);
+    }
+  }
+  else
+  {
+    printf("usage: non_precision_resistor_controller [bus] [device] [resistance]\n");
+  }
+
+  return 0;
+}
