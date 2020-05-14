21504d7f0b9fd66b0f0a2978952ed35f003a52e1
Dennis Lu <Dennis.Lu@itri.org.tw>
2020-05-14 14:56:24 +0800

    testing_main: added OpenCard() function

--- src/motor_control_unit/src/simulink_generated_code/testing_grt_rtw/testing_main.cpp
+++ src/motor_control_unit/src/simulink_generated_code/testing_grt_rtw/testing_main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include <memory>
 
 #include <testing.h>
 
+#include <RtTaskHandler.h>
+
+static auto rtTaskHandler = std::make_unique<RtTaskHandler>();
+
 int main(int argc, char **argv)
 {
+  DWORD cardNum = 3;
+  rtTaskHandler->OpenCard(cardNum);
+
   auto testingModel = testingModelClass();
   testingModel.initialize();
   for(auto i{0u}; i < 10u; ++i)
