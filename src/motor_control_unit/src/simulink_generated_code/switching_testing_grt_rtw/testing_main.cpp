442ffd43f86e6ebcc5e78329eeb75b4e540d835a
Dennis Lu <Dennis.Lu@itri.org.tw>
2020-05-18 14:48:14 +0800

    switching_testing_grt_rtw/testing_main: added RtSharedSwitchState and StartGenerateSwitchStateRoutine()

--- src/motor_control_unit/src/simulink_generated_code/switching_testing_grt_rtw/testing_main.cpp
+++ src/motor_control_unit/src/simulink_generated_code/switching_testing_grt_rtw/testing_main.cpp
@@ -9,50 +9,73 @@
 
 #include <RtTaskHandler.h>
 
+class RtSharedState
+{
+public:
+  BOOL mState;
+  RT_MUTEX mMutex;
+
+public:
+  RtSharedState()
+  {
+    rt_mutex_create(&mMutex, "RtSharedStateMutex");
+  }
+
+  void Set(bool b)
+  {
+    rt_mutex_acquire_until(&mMutex, TM_INFINITE);
+    mState = b;
+    rt_mutex_release(&mMutex);
+  }
+
+  BOOL Get()
+  {
+    rt_mutex_acquire_until(&mMutex, TM_INFINITE);
+    auto currentState = mState;
+    rt_mutex_release(&mMutex);
+    return currentState;
+  }
+};
+
+
+RT_TASK rtSwitchStateTask;
+
 //RT_TASK rtResistanceArrayTask;
 //
 //static auto rtSharedResistanceArray = std::make_shared<RtSharedResistanceArray>();
 //static auto rtTaskHandler = std::make_unique<RtTaskHandler>();
-//static auto testingModel = testingModelClass();
+static auto testingModel = testingModelClass();
+static RtSharedState rtSharedState;
 
-//void GenerateResistanceArrayRoutine(void*)
-//{
-//  std::vector<DWORD> resistances;
-//  testingModel.initialize();
-//
-//  while(true)
-//  {
-//    resistances.clear();
-//
-//    for(auto i{0u}; i < 10u; ++i)
-//    {
-//      testingModel.step();
-//
-//      DWORD subunit = i;
-//      DWORD resistance = testingModel.testing_Y.Out1 * 2+ 10;
-//
-//      resistances.push_back(resistance);
-//    }
-//
-//    rtSharedResistanceArray->SetArray(resistances);
-//
-//    rt_task_wait_period(NULL);
-//  }
-//}
-//
-//int StartGenerateResistanceArrayRoutine()
-//{
-//  int e1 = rt_task_create(&rtResistanceArrayTask, "GenerateResistanceArrayRoutine",
-//    kTaskStackSize, kMediumTaskPriority, kTaskMode);
-//  int e2 = rt_task_set_periodic(&rtResistanceArrayTask, TM_NOW, rt_timer_ns2ticks(1000000));
-//  int e3 = rt_task_start(&rtResistanceArrayTask, &GenerateResistanceArrayRoutine, NULL);
-//
-//  if(e1 | e2 | e3)
-//  {
-//    printf("Error launching StartSetSubunitResistanceRoutine. Exiting.\n");
-//    return -1;
-//  }
-//}
+
+void GenerateSwitchStateRoutine(void*)
+{
+  testingModel.initialize();
+
+  while(true)
+  {
+    testingModel.step();
+
+    rtSharedState.Set(!rtSharedState.Get());
+
+    rt_task_wait_period(NULL);
+
+    printf("state is: %s\n", rtSharedState.Get() ? "true" : "false");
+  }
+}
+
+int StartGenerateSwitchStateRoutine()
+{
+  int e1 = rt_task_create(&rtSwitchStateTask, "GenerateSwitchStateRoutine",
+    kTaskStackSize, kMediumTaskPriority, kTaskMode);
+  int e2 = rt_task_set_periodic(&rtSwitchStateTask, TM_NOW, rt_timer_ns2ticks(1000000));
+  int e3 = rt_task_start(&rtSwitchStateTask, &GenerateSwitchStateRoutine, NULL);
+
+  if(e1 | e2 | e3)
+  {
+    printf("Error launching StartGenerateSwitchStateRoutine(). Exiting.\n");
+  }
+}
 
 void TerminationHandler(int s)
 {
@@ -73,15 +96,15 @@ int main(int argc, char **argv)
   signalHandler.sa_flags = 0;
   sigaction(SIGINT, &signalHandler, NULL);
 
-//  StartGenerateResistanceArrayRoutine();
+  StartGenerateSwitchStateRoutine();
 //
 //  DWORD cardNum = 3;
 //  rtTaskHandler->OpenCard(cardNum);
 //  rtTaskHandler->mRtSharedResistanceArray = rtSharedResistanceArray;
 //  rtTaskHandler->StartSetSubunitResistanceRoutine();
 
-//  while(true) // original parent process will wait until ctrl+c signal
-//  {}
+  while(true) // original parent process will wait until ctrl+c signal
+  {}
 
   return 0;
 }
