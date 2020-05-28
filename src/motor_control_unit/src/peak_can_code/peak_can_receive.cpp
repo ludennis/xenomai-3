9b1e68f2eecc2c8a1b99ae00ea76169e9b07f98d
Dennis Lu <Dennis.Lu@itri.org.tw>
2020-05-28 17:18:55 +0800

    peak_can_code: successful transmission at baud rate 100 kbits/s from usb to pci

--- src/motor_control_unit/src/peak_can_code/peak_can_receive.cpp
+++ src/motor_control_unit/src/peak_can_code/peak_can_receive.cpp
@@ -1,6 +1,57 @@
+#include <errno.h>
+#include <stdio.h>
+
 #include <libpcan.h>
+
+/* print out the content of a CAN message */
+void print_message(const char *prompt, TPCANMsg *m)
+{
+  int i;
+
+  /* print RTR, 11 or 29, CAN-Id and datalength */
+  printf("%s: %c %c 0x%08x %1d ",
+      prompt,
+      (m->MSGTYPE & MSGTYPE_STATUS) ? 'x' :
+        ((m->MSGTYPE & MSGTYPE_RTR) ? 'r' : 'm') -
+        ((m->MSGTYPE & MSGTYPE_SELFRECEIVE) ? 0x20 : 0),
+      (m->MSGTYPE & MSGTYPE_STATUS) ? '-' :
+        ((m->MSGTYPE & MSGTYPE_EXTENDED) ? 'e' : 's'),
+       m->ID,
+       m->LEN);
+
+  /* don't print any telegram contents for remote frames */
+  if (!(m->MSGTYPE & MSGTYPE_RTR))
+    for (i = 0; i < m->LEN; i++)
+      printf("%02x ", m->DATA[i]);
+
+  printf("\n");
+}
 
 int main(int argc, char **argv)
 {
+  HANDLE canHandle = LINUX_CAN_Open("/dev/pcanusb32", 0);
+  errno = CAN_Init(canHandle, CAN_BAUD_100K, CAN_INIT_TYPE_EX);
+  if(errno)
+  {
+    perror("peak_can_receive: error with CAN_Init()");
+    return errno;
+  }
+  printf("Status = %i\n", CAN_Status(canHandle));
+
+
+  for(;;)
+  {
+    TPCANRdMsg readMsg;
+    if(LINUX_CAN_Read(canHandle, &readMsg))
+    {
+      perror("peak_can_receive: error with LINUX_CAN_READ()");
+      return errno;
+    }
+    printf("%u.%u ", readMsg.dwTime, readMsg.wUsec);
+    print_message("peak_can_receive", &(readMsg.Msg));
+  }
+
+  CAN_Close(canHandle);
+
   return 0;
 }
--- src/motor_control_unit/src/peak_can_code/peak_can_transmit.cpp
+++ src/motor_control_unit/src/peak_can_code/peak_can_transmit.cpp
@@ -1,6 +1,24 @@
+#include <stdio.h>
+
 #include <libpcan.h>
 
 int main(int argc, char **argv)
 {
+  HANDLE canHandle = LINUX_CAN_Open("/dev/pcanpcifd1", 0);
+  CAN_Init(canHandle, CAN_BAUD_100K, CAN_INIT_TYPE_EX);
+  printf("Status = %i\n", CAN_Status(canHandle));
+
+  TPCANMsg writeMsg;
+  writeMsg.ID = 0x123;
+  writeMsg.MSGTYPE = MSGTYPE_STANDARD;
+  writeMsg.LEN = 3;
+  writeMsg.DATA[0] = 0x1;
+  writeMsg.DATA[1] = 0x2;
+  writeMsg.DATA[2] = 0x3;
+
+  CAN_Write(canHandle, &writeMsg);
+
+  CAN_Close(canHandle);
+
   return 0;
 }
