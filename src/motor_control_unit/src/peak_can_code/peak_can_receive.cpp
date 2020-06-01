6dd06619752fffaddbe1b5e06028f0971d25bdf9
Dennis Lu <Dennis.Lu@itri.org.tw>
2020-06-01 14:30:43 +0800

    peak_can_receive/peak_can_transmit: added termination handler ctrl + c

--- src/motor_control_unit/src/peak_can_code/peak_can_receive.cpp
+++ src/motor_control_unit/src/peak_can_code/peak_can_receive.cpp
@@ -1,14 +1,32 @@
+#include <memory>
+
 #include <RtMacro.h>
 #include <RtPeakCanReceiveTask.h>
 
+std::unique_ptr<RtPeakCanReceiveTask> rtPeakCanReceiveTask;
+
+void TerminationHandler(int s)
+{
+  printf("Caught ctrl + c signal. Closing and Exiting.\n");
+  rtPeakCanReceiveTask.reset();
+  exit(1);
+}
+
 int main(int argc, char **argv)
 {
-  auto rtPeakCanReceiveTask = RtPeakCanReceiveTask(
+  // ctrl + c signal handler
+  struct sigaction signalHandler;
+  signalHandler.sa_handler = TerminationHandler;
+  sigemptyset(&signalHandler.sa_mask);
+  signalHandler.sa_flags = 0;
+  sigaction(SIGINT, &signalHandler, NULL);
+
+  auto rtPeakCanReceiveTask = std::make_unique<RtPeakCanReceiveTask>(
     "/dev/pcanusb32", CAN_BAUD_500K, "RtPeakCanReceiveTask", RtMacro::kTaskStackSize,
     RtMacro::kMediumTaskPriority, RtMacro::kTaskMode, RtMacro::kHundredMsTaskPeriod,
     RtMacro::kCoreId6);
 
-  rtPeakCanReceiveTask.StartRoutine();
+  rtPeakCanReceiveTask->StartRoutine();
 
   while(true)
   {}
--- src/motor_control_unit/src/peak_can_code/peak_can_transmit.cpp
+++ src/motor_control_unit/src/peak_can_code/peak_can_transmit.cpp
@@ -1,14 +1,32 @@
+#include <memory>
+
 #include <RtMacro.h>
 #include <RtPeakCanTransmitTask.h>
 
+std::unique_ptr<RtPeakCanTransmitTask> rtPeakCanTransmitTask;
+
+void TerminationHandler(int s)
+{
+  printf("Caught ctrl + c signal. Closing and Exiting.\n");
+  rtPeakCanTransmitTask.reset();
+  exit(1);
+}
+
 int main(int argc, char **argv)
 {
-  auto rtPeakCanTransmitTask =
-    RtPeakCanTransmitTask("/dev/pcanpcifd1", CAN_BAUD_500K, "RtPeakCanTransmitTask",
+  // ctrl + c signal handler
+  struct sigaction signalHandler;
+  signalHandler.sa_handler = TerminationHandler;
+  sigemptyset(&signalHandler.sa_mask);
+  signalHandler.sa_flags = 0;
+  sigaction(SIGINT, &signalHandler, NULL);
+
+  auto rtPeakCanTransmitTask = std::make_unique<RtPeakCanTransmitTask>(
+      "/dev/pcanpcifd1", CAN_BAUD_500K, "RtPeakCanTransmitTask",
       RtMacro::kTaskStackSize, RtMacro::kMediumTaskPriority, RtMacro::kTaskMode,
       RtMacro::kHundredMsTaskPeriod, RtMacro::kCoreId7);
 
-  rtPeakCanTransmitTask.StartRoutine();
+  rtPeakCanTransmitTask->StartRoutine();
 
   while(true)
   {}
