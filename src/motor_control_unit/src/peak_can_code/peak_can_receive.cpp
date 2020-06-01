67581f6f177a5458e88d951e2e4b3d84f4036152
Dennis Lu <Dennis.Lu@itri.org.tw>
2020-06-01 13:46:51 +0800

    peak_can_receive: RtPeriod changed to 100ms

--- src/motor_control_unit/src/peak_can_code/peak_can_receive.cpp
+++ src/motor_control_unit/src/peak_can_code/peak_can_receive.cpp
@@ -5,7 +5,7 @@ int main(int argc, char **argv)
 {
   auto rtPeakCanReceiveTask = RtPeakCanReceiveTask(
     "/dev/pcanusb32", CAN_BAUD_500K, "RtPeakCanReceiveTask", RtMacro::kTaskStackSize,
-    RtMacro::kMediumTaskPriority, RtMacro::kTaskMode, RtMacro::kTenMsTaskPeriod,
+    RtMacro::kMediumTaskPriority, RtMacro::kTaskMode, RtMacro::kHundredMsTaskPeriod,
     RtMacro::kCoreId6);
 
   rtPeakCanReceiveTask.StartRoutine();
