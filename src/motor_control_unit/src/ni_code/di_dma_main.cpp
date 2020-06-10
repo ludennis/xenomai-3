22cca084a6ec043d5986f85b40f6332a30f02b36
Dennis Lu <Dennis.Lu@itri.org.tw>
2020-06-10 14:36:11 +0800

    di_dma_main: compilable skeleton code added

---
+++ src/motor_control_unit/src/ni_code/di_dma_main.cpp
@@ -0,0 +1,11 @@
+#include <osiBus.h>
+#include <tXSeries.h>
+
+#include <dmaErrors.h>
+#include <dmaProperties.h>
+#include <tCHInChDMAChannel.h>
+
+int main(int argc, char *argv[])
+{
+  return 0;
+}
