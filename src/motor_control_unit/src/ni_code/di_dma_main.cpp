e3dcfc1345400a188ce7e65405c3f615942929e1
Dennis Lu <Dennis.Lu@itri.org.tw>
2020-06-15 16:04:34 +0800

    di_dma_main: removed unnecessary include headers

--- src/motor_control_unit/src/ni_code/di_dma_main.cpp
+++ src/motor_control_unit/src/ni_code/di_dma_main.cpp
@@ -1,12 +1,5 @@
 #include <DmaDigitalInputOutputTask.h>
 
-#include <osiBus.h>
-#include <tXSeries.h>
-
-#include <dmaErrors.h>
-#include <dmaProperties.h>
-#include <tCHInChDMAChannel.h>
-
 int main(int argc, char *argv[])
 {
   auto dmaDiTask = std::make_unique<DmaDigitalInputOutputTask>();
