cab58a7f423c06ff4b7985b68b4c50dc35e6372f
Dennis Lu <Dennis.Lu@itri.org.tw>
2020-06-15 15:17:56 +0800

    di_dma_main: reads value from DMA

--- src/motor_control_unit/src/ni_code/di_dma_main.cpp
+++ src/motor_control_unit/src/ni_code/di_dma_main.cpp
@@ -11,6 +11,12 @@ int main(int argc, char *argv[])
 {
   auto dmaDiTask = std::make_unique<DmaDigitalInputOutputTask>();
   dmaDiTask->Init(argv[1], argv[2]);
+  dmaDiTask->StartDmaChannel();
+  dmaDiTask->EnableStreamHelper();
+  dmaDiTask->ProgramDiSubsystem(10000u, 2u);
+  dmaDiTask->ArmAndStartDiSubsystem();
+  dmaDiTask->DmaRead();
+  dmaDiTask->Stop();
 
   return 0;
 }
