0d4a3e4a21f045e43eb1d9107de1c0169de2389a
Dennis Lu <Dennis.Lu@itri.org.tw>
2020-06-16 10:03:34 +0800

    di_dma_main: added ctrl + c termination

--- src/motor_control_unit/src/ni_code/di_dma_main.cpp
+++ src/motor_control_unit/src/ni_code/di_dma_main.cpp
@@ -1,15 +1,39 @@
+#include <signal.h>
+#include <chrono>
+#include <thread>
+
 #include <DmaDigitalInputOutputTask.h>
+
+std::unique_ptr<DmaDigitalInputOutputTask> dmaDiTask;
+
+void terminationHandler(int signal)
+{
+  dmaDiTask->Stop();
+  dmaDiTask.reset();
+  exit(1);
+}
 
 int main(int argc, char *argv[])
 {
-  auto dmaDiTask = std::make_unique<DmaDigitalInputOutputTask>();
+  struct sigaction action;
+  action.sa_handler = terminationHandler;
+  action.sa_flags = 0;
+  sigemptyset(&action.sa_mask);
+  sigaction(SIGINT , &action, NULL);
+
+  dmaDiTask = std::make_unique<DmaDigitalInputOutputTask>();
   dmaDiTask->Init(argv[1], argv[2]);
   dmaDiTask->StartDmaChannel();
   dmaDiTask->EnableStreamHelper();
   dmaDiTask->ProgramDiSubsystem(10000u, 2u);
   dmaDiTask->ArmAndStartDiSubsystem();
-  dmaDiTask->DmaRead();
-  dmaDiTask->Stop();
+
+  for (;;)
+  {
+    dmaDiTask->DmaRead();
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+  }
+//  dmaDiTask->Stop();
 
   return 0;
 }
