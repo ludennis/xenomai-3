d603aaeedbcd6198c7e4874169a115e29d7614be
Dennis Lu <Dennis.Lu@itri.org.tw>
2020-07-13 11:38:15 +0800

    to_rt_pipe: added skeleton code that includes carla_client_server_user.idl

---
+++ src/motor_control_unit/src/dds_code/from_rt_pipe.cpp
@@ -0,0 +1,42 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <iostream>
+#include <thread>
+
+#include <MessageTypes.h>
+#include <RtMacro.h>
+
+int main(int argc, char *argv[])
+{
+  auto fileDescriptor = open("/dev/rtp1", O_RDONLY);
+
+  if (fileDescriptor < 0)
+    printf("file descriptor error: %s\n", strerror(errno));
+  else
+    printf("file descriptor acquired\n");
+
+  for (;;)
+  {
+    MotorOutputMessage *motorOutputMessage =
+      (MotorOutputMessage*) malloc(RtMessage::kMessageSize);
+    auto bytesRead = read(fileDescriptor, motorOutputMessage, RtMessage::kMessageSize);
+    printf("Read bytes %ld from fileDescriptor\n", bytesRead);
+    if (bytesRead > 0)
+    {
+      printf("motorOutputMessage rpm: %f\n", motorOutputMessage->ft_RotorRPM);
+    }
+    if (bytesRead < 0)
+      printf("read error: %s\n", strerror(errno));
+    free(motorOutputMessage);
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+  }
+  close(fileDescriptor);
+
+  return 0;
+}
---
+++ src/motor_control_unit/src/dds_code/to_rt_pipe.cpp
@@ -0,0 +1,6 @@
+#include <gen/carla_client_server_user_DCPS.hpp>
+
+int main(int argc, char *argv[])
+{
+  return 0;
+}
