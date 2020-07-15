3b82fc9fb0ea159150a53dc8dde3d4b5882f00b7
Dennis Lu <Dennis.Lu@itri.org.tw>
2020-07-15 09:37:22 +0800

    to_rt_pipe: added termination handler and fixed error when received bad message

--- src/motor_control_unit/src/dds_code/to_rt_pipe.cpp
+++ src/motor_control_unit/src/dds_code/to_rt_pipe.cpp
@@ -1,3 +1,4 @@
+#include <signal.h>
 #include <unistd.h>
 
 #include <chrono>
@@ -12,10 +13,27 @@
 #include <MessageTypes.h>
 #include <RtMacro.h>
 
+int fileDescriptor;
+void *buffer;
+
+void TerminationHandler(int signal)
+{
+  printf("Termination signal received. Exiting ...\n");
+  free(buffer);
+  close(fileDescriptor);
+  exit(1);
+}
+
 int main(int argc, char *argv[])
 {
+  struct sigaction action;
+  action.sa_handler = TerminationHandler;
+  sigemptyset(&action.sa_mask);
+  action.sa_flags = 0;
+  sigaction(SIGINT, &action, NULL);
+
   char deviceName[] = "/dev/rtp0";
-  auto fileDescriptor = open(deviceName , O_WRONLY | O_NONBLOCK);
+  fileDescriptor = open(deviceName , O_WRONLY | O_NONBLOCK);
   if (fileDescriptor < 0)
   {
     printf("file descriptor error opening %s\n", deviceName);
@@ -42,10 +60,9 @@ int main(int argc, char *argv[])
   ddsBridge.EnableStatus(statusCondition, dds::core::status::StatusMask::data_available());
   ddsBridge.AddStatusCondition(statusCondition);
 
-  void *buffer = malloc(RtMessage::kMessageSize);
+  buffer = malloc(RtMessage::kMessageSize);
   for (;;)
   {
-    printf("waiting for messages ...\n");
     auto conditions = ddsBridge.mWaitSet.wait();
     for (auto i{0u}; i < conditions.size(); ++i)
     {
@@ -57,13 +74,20 @@ int main(int argc, char *argv[])
         {
           const auto &sample = *itr;
           const auto sampleData = sample.data();
+
+          // TODO: or check validity of message
+          if (sampleData.simulation_time().empty())
+          {
+            continue;
+          }
+
           printf("sample.id = %d, sample.throttle = %f, sample.vehicle_speed = %f, "
             "sample.simulation_time = %s\n", sampleData.id(), sampleData.throttle(),
             sampleData.vehicle_speed(), sampleData.simulation_time().c_str());
 
+
           auto timeNow = std::chrono::duration_cast<std::chrono::nanoseconds>(
             std::chrono::steady_clock::now().time_since_epoch()).count();
-          printf("timeNow: %ld\n", timeNow);
 
           printf("DDS Message travel time: %ld ns\n",
             timeNow - std::stoul(sampleData.simulation_time()));
@@ -73,22 +97,15 @@ int main(int argc, char *argv[])
 
           // and send it to pipe /dev/rtp0
           memcpy(buffer, &motorInputMessage, RtMessage::kMessageSize);
+          printf("memcpy ed\n");
           auto bytesWritten = write(fileDescriptor, buffer, RtMessage::kMessageSize);
-          if (bytesWritten <= 0)
-          {
-            printf("write error: %s\n", strerror(errno));
-          }
-          else
-          {
-            printf("Written %ld bytes into pipe", bytesWritten);
-          }
-
+          printf("Written %ld bytes\n", bytesWritten);
+          // TODO: fix weird behavior of not going into the following if statements
+          // and also receiving an empty message when throttle_emulator quits
         }
       }
     }
   }
 
-  free(buffer);
-  close(fileDescriptor);
   return 0;
 }
