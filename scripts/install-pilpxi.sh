fc55ccce3673a936f0406480da3a9567cbb514c6
Dennis Lu <Dennis.Lu@itri.org.tw>
2021-04-26 14:22:53 +0800

    scripts/install-pilpxi.sh: added to install pilpxi driver

---
+++ scripts/install-pilpxi.sh
@@ -0,0 +1,6 @@
+cd ~
+git clone http://mmslswdev.itri.org.tw/bitbucket/scm/~a70572/pickering-linux-driver.git
+cd pickering-linux-driver
+sudo ./install.sh
+sudo rm -rf pickering-linux-driver
+echo 'Pilpxi driver has been installed in /usr/local/lib'
